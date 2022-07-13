41a0c8e1449113637fc51e4e12b8866a67ed302e
Home <88548702+Featheryxy@users.noreply.github.com>
2022-07-13 23:20:27 +0800

    stream

---
+++ src/main/java/ind/milo/demo/functionprogram/Test.java
@@ -0,0 +1,15 @@
+package ind.milo.demo.functionprogram;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Test {
+    Path path;
+    Paths paths;
+    Collection collection;
+    Collections collections;
+    List list;
+}
