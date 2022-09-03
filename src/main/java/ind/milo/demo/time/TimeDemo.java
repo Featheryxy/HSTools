f09a1228b1673766bc63939d2f19929665f57f87
Home <88548702+Featheryxy@users.noreply.github.com>
2022-09-03 12:17:08 +0800

    Create TimeDemo.java

---
+++ src/main/java/ind/milo/demo/time/TimeDemo.java
@@ -0,0 +1,30 @@
+package ind.milo.demo.time;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.time.*;
+
+@Slf4j
+public class TimeDemo {
+    @Test
+    public void now() {
+        Instant now = Instant.now();
+        System.out.println(now);
+        Instant plusSeconds = now.plusSeconds(60);
+        System.out.println(plusSeconds);
+
+        Duration between = Duration.between(now, plusSeconds);
+        System.out.println(between.toMinutes());
+    }
+
+    @Test
+    public void date() {
+        LocalDate now = LocalDate.now();
+        log.info(String.valueOf(now));
+
+        LocalDate date = LocalDate.of(2014, 1, 1).plusDays(255);
+        log.info(String.valueOf(date));
+        DayOfWeek.FRIDAY
+    }
+}
