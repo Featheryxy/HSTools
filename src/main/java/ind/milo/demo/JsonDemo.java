da413ffd9c1feb5b5d48c2c8f0753054cecdbd7d
HP-ELITEBOOK <88548702+Featheryxy@users.noreply.github.com>
2022-10-08 18:10:08 +0800

    Freemarker

--- src/main/java/ind/milo/demo/JsonDemo.java
+++ src/main/java/ind/milo/demo/JsonDemo.java
@@ -1,4 +1,4 @@
-package ind.milo.demo.javafxdemo;
+package ind.milo.demo;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
---
+++ src/main/java/ind/milo/util/FreemarkerUtil.java
@@ -0,0 +1,13 @@
+package ind.milo.util;
+
+/**
+ * @Date 2022/10/8 17:01
+ * @Created by Milo
+ */
+public class FreemarkerUtil {
+    private FreemarkerUtil() {
+
+    }
+
+
+}
