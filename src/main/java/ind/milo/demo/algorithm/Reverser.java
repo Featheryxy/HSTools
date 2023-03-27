b90bcb34752c54f246954c89c899f89c57d2c16f
Home <88548702+Featheryxy@users.noreply.github.com>
2023-03-27 23:11:04 +0800

    Create Reverser.java

---
+++ src/main/java/ind/milo/demo/algorithm/Reverser.java
@@ -0,0 +1,13 @@
+package ind.milo.demo.algorithm;
+
+import java.util.HashSet;
+
+public class Reverser {
+    public static void main(String[] args) {
+//        char A = 'A';
+//        System.out.println((byte)A);
+
+        byte A = 0;
+        System.out.println((char) A);
+    }
+}
