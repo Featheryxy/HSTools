83ffd39f52137c4c3d047335fd3b3eee8b970f5a
HP-ELITEBOOK <88548702+Featheryxy@users.noreply.github.com>
2023-04-04 09:01:23 +0800

    ArrayDequeDemo

---
+++ src/main/java/ind/milo/demo/algorithm/ArrayDemo.java
@@ -0,0 +1,14 @@
+package ind.milo.demo.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * @Date 2023/4/3 11:12
+ * @Created by Milo
+ */
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a >> 1);
+    }
+}
---
+++ src/main/java/ind/milo/demo/algorithm/ArrayDequeDemo.java
@@ -0,0 +1,15 @@
+package ind.milo.demo.algorithm;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Date 2023/4/3 11:18
+ * @Created by Milo
+ */
+public class ArrayDequeDemo {
+    public static void main(String[] args) {
+        ArrayDeque<String> arrayDeque = new ArrayDeque<>();
+    }
+}
