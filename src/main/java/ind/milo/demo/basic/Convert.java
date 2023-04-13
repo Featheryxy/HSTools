2343b59d2ee1d3081d72522b13b97c9267fb6872
Home <88548702+Featheryxy@users.noreply.github.com>
2023-04-13 23:32:59 +0800

    Create Convert.java

---
+++ src/main/java/ind/milo/demo/basic/Convert.java
@@ -0,0 +1,12 @@
+package ind.milo.demo.basic;
+
+public class Convert {
+    public static void main(String[] args) {
+        byte a = 127;
+        byte b = 127;
+        a = (byte) (a + b);
+//        a = (byte)(a + b);
+//        a +=b;
+
+    }
+}
