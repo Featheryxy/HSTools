0ab0136143d6a19de4859147e84de4f633386b5f
HP-ELITEBOOK <88548702+Featheryxy@users.noreply.github.com>
2023-05-30 20:07:23 +0800

    extends

--- src/main/java/ind/milo/demo/basic/Child.java
+++ src/main/java/ind/milo/demo/basic/Child.java
@@ -1,12 +1,39 @@
 package ind.milo.demo.basic;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 /**
  * @Date 2023/5/16 12:58
  * @Created by Milo
  */
 public class Child extends Person {
+    String name="child";
+
+    public Child(){
+        System.out.println("Child Instance");
+    }
+
+    public void sayFatherName(){
+        System.out.println("Child: "+super.name);
+    }
+
+//    @Override
+//    public void sayName() {
+//        System.out.println("Child's name: "+name);
+//    }
+
     public static void main(String[] args) {
-        Person person = new Child();
-        Long a = 1l;
+        // 创建子类的同时也会创建父类，并且将父类
+        // 但是父类创建成功后会直接销毁。
+        Child child = new Child();
+        child.sayFatherName();
+        System.out.println(child.name);
+        Method[] methods = Child.class.getMethods();
+        System.out.println("methods len: "+methods.length+" methods: "+Arrays.toString(methods));
+        Method[] declaredMethods = Child.class.getDeclaredMethods();
+        System.out.println("declare methods len: "+declaredMethods.length+" methods: "+Arrays.toString(declaredMethods));
+
+        System.out.println();
     }
 }
--- src/main/java/ind/milo/demo/basic/Person.java
+++ src/main/java/ind/milo/demo/basic/Person.java
@@ -5,9 +5,18 @@ package ind.milo.demo.basic;
  * @Created by Milo
  */
 public class Person {
-    private String name = "Person";
+    private String personName;
 
-    public static void main(String[] args) {
+    // 该成员变量可以被子类继承，但是由于其访问控制符为private, 所以子类不能直接访问该成员变量。
+    // 但是可以通过其他非private 方法来间接访问该成员变量
+    // 如果子类也声明了同样名称的
+    String name = "father";
 
+    public Person(){
+        System.out.println("Person Instance");
+    }
+
+    public void sayName(){
+        System.out.println("Person's name: "+name);
     }
 }
