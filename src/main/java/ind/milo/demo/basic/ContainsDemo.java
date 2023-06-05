a96686fba3c3600504bfa52a2146d3e89f81f7fb
HP-ELITEBOOK <88548702+Featheryxy@users.noreply.github.com>
2023-06-05 14:11:24 +0800

    Create ContainsDemo.java

---
+++ src/main/java/ind/milo/demo/basic/ContainsDemo.java
@@ -0,0 +1,12 @@
+package ind.milo.demo.basic;
+
+/**
+ * @Date 2023/5/31 13:17
+ * @Created by Milo
+ */
+public class ContainsDemo {
+    public static void main(String[] args) {
+        System.out.println("J1405, J1402".contains("J1405"));
+        System.out.println("J1405, J1402".contains("J1403"));
+    }
+}
