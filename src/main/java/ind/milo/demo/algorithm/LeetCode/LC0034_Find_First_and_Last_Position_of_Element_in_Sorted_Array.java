9080ea5628f0bdaeabe2f983c00b8761cc6a54e9
Home <88548702+Featheryxy@users.noreply.github.com>
2023-06-22 15:36:31 +0800

    delete package basic

--- src/main/java/ind/milo/demo/algorithm/LeetCode/LC0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
+++
@@ -1,8 +0,0 @@
-package ind.milo.demo.algorithm.LeetCode;
-
-/**
- * @Date 2023/4/27 18:20
- * @Created by Milo
- */
-public class LC0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array {
-}
--- src/main/java/ind/milo/demo/basic/IfDemo.java
+++
@@ -1,7 +0,0 @@
-package ind.milo.demo.basic;
-
-public class IfDemo {
-    public static void main(String[] args) {
-
-    }
-}
